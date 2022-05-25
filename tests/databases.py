1555dad42c453d4e8fd8bbd44d3992d39d3f7488
Jason <thebosssg19@gmail.com>
2022-05-25 16:47:26 -0400

    feat: made the test classes more uniform'
    note: everything works now

--- tests/databases.py
+++ tests/databases.py
@@ -2,13 +2,11 @@ import secrets
 import unittest
 from configparser import ConfigParser
 
-unittest.TestLoader.sortTestMethodsUsing = None
-
 from databases import grouper
 
 config = ConfigParser()
 config.read('../config.ini')
-TESTKEY = 'MmEKhQlTGCQSEDzBJQRqNg' #secrets.token_urlsafe(16)
+TESTKEY = secrets.token_urlsafe(16)
 TESTVALUE = "test value"
 
 
@@ -18,32 +16,30 @@ class PostGreSQL(unittest.TestCase):
 
     def main(self):
         self.TESTKEY = TESTKEY
-        if config[self.__class__.__name__.lower()]['Uri'] is None:  # the __class__.__name__ is the name of the class
-            raise self.skipTest("PostgreSQL Uri not found in config.ini")
-        self.PostGreDB = grouper.Database((config['setup']['DatabaseChoice']), 'tests').db
-        if self.PostGreDB.get_value(self.TESTKEY) is None:  # if the key doesn't exit in the database
+        self.db_name = self.__class__.__name__
+        if config[self.db_name.lower()]['Uri'] is None:  # the __class__.__name__ is the name of the class
+            raise self.skipTest(f"{self.db_name} Uri not found in config.ini")
+        self.db = grouper.Database((config['setup']['DatabaseChoice']), 'tests').db
+        if self.db.get_value(self.TESTKEY) is None:  # if the key doesn't exit in the database
             self.test_a_insert()
             self.test_delete()
         else:  # if the key does exist in the database, pick a new key
             self.TESTKEY = secrets.token_urlsafe(16)
 
-    def tearDown(self):  # delete the test values
-        self.PostGreDB.delete(self.TESTKEY)
-
     def test_a_insert(self):
         """Tests if the database can insert a value"""
-        self.PostGreDB.insert((self.TESTKEY, TESTVALUE))
-        self.assertEqual(self.PostGreDB.get_value(self.TESTKEY),
+        self.db.insert((self.TESTKEY, TESTVALUE))
+        self.assertEqual(self.db.get_value(self.TESTKEY),
                          TESTVALUE)  # get_value is a method of the Database class
 
     def test_delete(self):
         """tests if the database can delete a value"""
-        print(self.PostGreDB.exists(self.TESTKEY))
-        if self.PostGreDB.exists(self.TESTKEY):
-            self.PostGreDB.delete(self.TESTKEY)
-            self.assertFalse(self.PostGreDB.get_value(self.TESTKEY))
+        if self.db.exists(self.TESTKEY):
+            self.db.delete(self.TESTKEY)
+            self.assertFalse(self.db.get_value(self.TESTKEY))
         else:
-            return self.skipTest("The key doesn't exist in the database")
+            self.db.insert((self.TESTKEY, TESTVALUE))
+            self.test_delete()
 
 
 class MongoDB(unittest.TestCase):
@@ -52,30 +48,32 @@ class MongoDB(unittest.TestCase):
 
     def main(self):
         self.TESTKEY = TESTKEY
-        if config[self.__class__.__name__.lower()]['Uri'] is None:  # the __class__.__name__ is the name of the class
-            raise self.skipTest("MongoDB Uri not found in config.ini")
-        self.MongoDB = grouper.Database((config['setup']['DatabaseChoice']), 'tests').db
-        if self.MongoDB.get_value(self.TESTKEY) is None:  # if the key doesn't exit in the database
+        self.db_name = self.__class__.__name__
+        if config[self.db_name.lower()]['Uri'] is None:  # the __class__.__name__ is the name of the class.
+            raise self.skipTest(f"{self.db_name} Uri not found in config.ini")
+        self.db = grouper.Database((config['setup']['DatabaseChoice']), 'tests').db
+        if self.db.get_value(self.TESTKEY) is None:  # if the key doesn't exit in the database
             self.test_a_insert()
             self.test_delete()
         else:  # if the key does exist in the database, pick a new key
             self.TESTKEY = secrets.token_urlsafe(16)
 
     def tearDown(self):  # delete the test values
-        self.MongoDB.delete(self.TESTKEY)
+        self.db.delete(self.TESTKEY)
 
     def test_a_insert(self):
         """Tests if the database can insert a value"""
-        self.MongoDB.insert((self.TESTKEY, TESTVALUE))
-        self.assertEqual(self.MongoDB.get_value(self.TESTKEY), TESTVALUE)  # get_value is a method of the Database class
+        self.db.insert((self.TESTKEY, TESTVALUE))
+        self.assertEqual(self.db.get_value(self.TESTKEY), TESTVALUE)  # get_value is a method of the Database class
 
     def test_delete(self):
         """tests if the database can delete a value"""
-        if self.MongoDB.exists(self.TESTKEY):
-            self.MongoDB.delete(self.TESTKEY)
-            self.assertFalse(self.MongoDB.get_value(self.TESTKEY))
+        if self.db.exists(self.TESTKEY):
+            self.db.delete(self.TESTKEY)
+            self.assertFalse(self.db.get_value(self.TESTKEY))
         else:
-            pass  # TODO add a exists method to the database class
+            self.db.insert((self.TESTKEY, TESTVALUE))
+            self.test_delete()
 
 
 if __name__ == '__main__':
